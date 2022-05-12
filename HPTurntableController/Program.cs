20291c12cf4e4fe860257ec1a0ab167f680d3b53
Adnan Üzülmez <56755924+adnanuzulmez@users.noreply.github.com>
2022-05-12 15:59:57 +0300

    Update Program.cs

--- HPTurntableController/Program.cs
+++ HPTurntableController/Program.cs
@@ -1,8 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: HPTurntableController.Program
-// Assembly: HPTurntableController, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 71B4AD93-2530-48B6-A110-5AECD4E2B667
-// Assembly location: C:\Users\adnan\Desktop\HPTurntableController.exe
+
 
 using System;
 using System.Windows.Forms;
